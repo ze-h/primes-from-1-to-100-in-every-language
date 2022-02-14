c8a8d7cade5bc93227780235ddc745ea65321b3e
ze-h <chilledlegumes@aol.com>
2022-02-14 13:17:23 -0500

    go

---
+++ primes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func prime(x int) bool {
+	for i := x - 1; i >= 2; i-- {
+		if x%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+	for i := 0; i < 100; i++ {
+		if prime(i) {
+			fmt.Println(i)
+		}
+	}
+}
