261639bb81631b6e295171ec2c5f21cf9ac234c2
ze-h <chilledlegumes@aol.com>
2022-02-18 08:59:59 -0500

    go

--- primes.go
+++ primes.go
@@ -3,17 +3,17 @@ package primes
 import ("fmt")
 
 func prime(x int) bool {
-	for i := x - 1; i >= 2; i-- {
-		if x%i == 0 {
-			return false
-		}
-	}
-	return true
+    for i := x - 1; i >= 2; i-- {
+        if x%i == 0 {
+            return false
+        }
+    }
+    return true
 }
 func main() {
-	for i := 0; i < 100; i++ {
-		if prime(i) {
-			fmt.Println(i)
-		}
-	}
+    for i := 0; i < 100; i++ {
+        if prime(i) {
+            fmt.Println(i)
+        }
+    }
 }
