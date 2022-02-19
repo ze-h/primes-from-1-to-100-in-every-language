f63c27c4d95800db5da924f8719ee07545ebe58b
ze-h <chilledlegumes@aol.com>
2022-02-18 22:25:45 -0500

    hs

--- primes.hs
+++ primes.hs
@@ -1,18 +1,18 @@
 module Main where
 
---loopr goes backwards from n -> 2
-loopr :: (Integer, Integer) -> IO()
-loopr (1, m) = print m
-loopr (n, m) = do
+--loopb goes backwards from n -> 2
+loopb :: (Integer, Integer) -> IO()
+loopb (1, m) = print m
+loopb (n, m) = do
     if mod m n == 0
         then return ()
-    else loopr (n-1, m)
+    else loopb (n-1, m)
 
 --loopf goes forwards from n -> 100
 loopf :: Integer -> IO ()
 loopf 101 = return ()
 loopf n = do
-    loopr (n-1, n)
+    loopb (n-1, n)
     loopf (n+1)
 
 main :: IO()
