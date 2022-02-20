c84c1653266c79ccdd426cc03806cea50ddfce91
ze-h <chilledlegumes@aol.com>
2022-02-20 03:03:25 -0500

    monad time

--- primes.hs
+++ primes.hs
@@ -1,11 +1,9 @@
 module Main where
+import Control.Monad(when)
 
 loopb :: (Integer, Integer) -> IO()
 loopb (1, m) = print m
-loopb (n, m) = do
-    if mod m n == 0
-        then return ()
-    else loopb (n-1, m)
+loopb (n, m) = when (mod m n /= 0) (loopb (n-1, m))
 
 loopf :: Integer -> IO ()
 loopf 101 = return ()
