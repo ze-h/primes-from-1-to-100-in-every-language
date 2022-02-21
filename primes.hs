c5be351a8d0893f6c3375aa5271e3fd3d13a4f2c
ze-h <chilledlegumes@aol.com>
2022-02-20 20:17:29 -0500

    otherwise

--- primes.hs
+++ primes.hs
@@ -2,11 +2,12 @@ module Main where
 import Control.Monad(when)
 
 loopb :: (Integer, Integer) -> IO()
-loopb (1, m) = print m
-loopb (n, m) = when (mod m n /= 0) (loopb (n-1, m))
+loopb (n, m)
+    | n == 1 = print m
+    | otherwise = when (mod m n /= 0) (loopb (n-1, m))
 
 loopf :: Integer -> IO ()
-loopf 101 = return ()
+loopf 101 = return()
 loopf n = do
     loopb (n-1, n)
     loopf (n+1)
