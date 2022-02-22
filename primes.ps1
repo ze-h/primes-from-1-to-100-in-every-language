28789b20412c02a105df9415ca1143c0eaeecec2
ze-h <chilledlegumes@aol.com>
2022-02-22 17:15:31 -0500

    ps1

--- primes.ps1
+++ primes.ps1
@@ -1,9 +1,9 @@
 function main() {
     foreach ($_ in 2..100){
-        if (isPrime2($_) -eq 1) { Write-Host $_ }
+        if (isPrime($_) -eq 1) { Write-Host $_ }
     }
 }
-function isPrime2($n) {
+function isPrime($n) {
     for ($i = 2; $i -le $n/2; $i++) {
         if($n%$i -eq0){return 0;}
     }
