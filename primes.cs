a25ab6644ef707afd71a36ee63231307696a6127
ze-h <chilledlegumes@aol.com>
2022-02-22 14:40:00 -0500

    top-level

--- primes.cs
+++ primes.cs
@@ -1,16 +1,12 @@
 using System;
-class primes{
-    public static bool prime(int x){
-        for (int i = x / 2; i >= 2 ; i--)
-        {
-            if(x%i==0){return false;}
-        }
-        return true;
+bool prime(int x){
+    for (int i = x / 2; i >= 2 ; i--){
+        if(x%i==0){return false;}
     }
-    public static void Main(string[] args){
-        for (var i = 2; i <= 100; i++)
-        {
-            if(prime(i)){Console.WriteLine(i);}
-        }
+    return true;
+}
+void Main(string[] args){
+    for (var i = 2; i <= 100; i++){
+        if(prime(i)){Console.WriteLine(i);}
     }
 }
