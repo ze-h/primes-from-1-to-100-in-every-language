373470d52e09e843ae16c2882cfd1b235e4792ef
ze-h <67851652+ze-h@users.noreply.github.com>
2022-02-22 17:20:56 -0500

    less go(lang)
    i pull up

--- primes.go
+++ primes.go
@@ -1,13 +1,13 @@
 package prime
 import ("fmt")
 func prime(x int) bool {
-    for i := x / 2; i >= 2; i-- {
+    for (i := x / 2; i >= 2; i--){
         if (x%i == 0){ return false }
     }
     return true
 }
 func main() {
-    for i := 2; i < 100; i++ {
+    for (i := 2; i < 100; i++){
         if (prime(i)){ fmt.Println(i) }
     }
 }
