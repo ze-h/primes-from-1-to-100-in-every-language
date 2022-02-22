634f2ac0cde0db6678d9e397058f19ebc0335eb1
ze-h <chilledlegumes@aol.com>
2022-02-22 16:53:19 -0500

    slightly less gross

--- primes but worse.hs
+++ primes but worse.hs
@@ -6,8 +6,5 @@ loopb (n, m)
     | n == 1 = print m
     | otherwise = when (mod m n /= 0) (loopb (n-1, m))
 
-sloop :: Integer -> IO()
-sloop n = loopb(div n 2, n)
-
 main :: IO()
-main = forM_ [[2..100] :: [Integer]] $ \n -> forM_ n sloop
+main = forM_ ([2..100]::[Integer]) $ \n -> loopb(div n 2, n)
