808b47471c1515d37d591330c66a0ba446873a24
ze-h <chilledlegumes@aol.com>
2022-02-23 09:31:43 -0500

    removed comments

--- primes.fs
+++ primes.fs
@@ -1,5 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-let prime (x : int) : bool =
+﻿let prime (x : int) : bool =
     let mutable flag = true
     for i in 2 .. 1 .. (x/2) do
         if x % i = 0 then flag <- false
