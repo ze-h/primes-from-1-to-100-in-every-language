5af6e907e902a35fc2b3884aab041d79947e4eb6
ze-h <chilledlegumes@aol.com>
2022-03-03 09:46:14 -0500

    optimization

--- primes.hs
+++ primes.hs
@@ -5,4 +5,4 @@ loopb n m
     | n == 1 = print m
     | otherwise = when (mod m n /= 0) (loopb (n - 1) m)
 main :: IO()
-main = forM_([2..100]::[Integer]) $ \n -> loopb(div n 2) n
+main = forM_([2..100]::[Integer]) (\n -> loopb(div n 2) n)
--- primes.py
+++ primes.py
@@ -1,8 +1,6 @@
 def prime(x):
     for i in range(int(x / 2),1,-1):
-        if x%i == 0:
-            return False
+        if x%i == 0: return False
     return True
 for i in range(2,100):
-    if prime(i) == True:
-        print(i)
+    if prime(i) == True: print(i)
