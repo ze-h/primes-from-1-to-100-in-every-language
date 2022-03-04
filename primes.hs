4d628b334a4f862fe9fe1c84b8f25790f49ee5a1
ze-h <chilledlegumes@aol.com>
2022-03-04 09:57:21 -0500

    int

--- primes.hs
+++ primes.hs
@@ -1,8 +1,8 @@
 module Main where
-import Control.Monad(when, forM_)
-loopb :: Integer -> Integer -> IO()
+import Control.Monad
+loopb :: Int -> Int -> IO()
 loopb n m
     | n == 1 = print m
     | otherwise = when (mod m n /= 0) (loopb (n - 1) m)
 main :: IO()
-main = forM_([2..100]::[Integer]) (\n -> loopb(div n 2) n)
+main = forM_ [2..100] (\n -> loopb(div n 2) n)
