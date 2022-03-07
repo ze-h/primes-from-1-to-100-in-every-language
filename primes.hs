5f9ebdada91e406cd65a38da3677085ae30d8b19
ze-h <chilledlegumes@aol.com>
2022-03-07 12:34:23 -0500

    removed if

--- primes.hs
+++ primes.hs
@@ -3,6 +3,7 @@ import Control.Monad ( forM_, when )
 loopb :: Int -> Int -> Bool 
 loopb n m
     | n == 1 = True
-    | otherwise = if mod m n /= 0 then loopb (n - 1) m else False
+    | mod m n /= 0 = loopb (n - 1) m
+    | otherwise = False
 main :: IO()
 main = forM_ [2..100] (\n -> when(loopb(div n 2) n)(print n))
