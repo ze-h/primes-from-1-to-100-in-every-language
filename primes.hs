37641c2fcbf69730e735a01404af9809f66d9f5b
ze-h <chilledlegumes@aol.com>
2022-03-08 08:37:21 -0500

    the baby (let us commence forth)

--- primes.hs
+++ primes.hs
@@ -1,9 +1,9 @@
 module Main where
 import Control.Monad ( forM_, when )
-loopb :: Int -> Int -> Bool 
-loopb n m
+isPrime :: Int -> Int -> Bool 
+isPrime n m
     | n == 1 = True
-    | mod m n /= 0 = loopb (n - 1) m
+    | mod m n /= 0 = isPrime (n - 1) m
     | otherwise = False
 main :: IO()
-main = forM_ [2..100] (\n -> when(loopb(div n 2) n)(print n))
+main = forM_ [2..100] (\n -> when(isPrime(div n 2) n)(print n))
