4c597dbd7b10a801b30567f0e8d5aefd25a992d4
ze-h <chilledlegumes@aol.com>
2022-03-09 09:44:49 -0500

    reverse, reverse!

--- primes.hs
+++ primes.hs
@@ -2,8 +2,8 @@ module Main where
 import Control.Monad ( forM_, when )
 isPrime :: Int -> Int -> Bool 
 isPrime n m
-    | n == 1 = True
-    | mod m n /= 0 = isPrime (n - 1) m
+    | m == 1 = True
+    | mod n m /= 0 = isPrime n (m - 1)
     | otherwise = False
 main :: IO()
-main = forM_ [2..100] (\n -> when(isPrime(div n 2) n)(print n))
+main = forM_ [2..100] (\n -> when(isPrime n (div n 2))(print n))
