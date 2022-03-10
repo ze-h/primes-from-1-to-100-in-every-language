57ebb7acdb8ecb75999a9dea31db66a836c4ddb0
ze-h <chilledlegumes@aol.com>
2022-03-10 11:18:23 -0500

    simplified lambda

--- primes.py
+++ primes.py
@@ -1,3 +1,3 @@
-prime = lambda x,y : y == 1 or (False if (x % y == 0) else prime(x, y-1))
-for i in range(2,100):
+prime = lambda x,y : y == 1 or (x % y != 0 and prime(x, y-1))
+for i in range(2,100): 
     if prime(i, int(i/2)): print(i)
