aee55fea9da1a8677ea1a85a0e14eaaac47bd549
ze-h <chilledlegumes@aol.com>
2022-03-31 09:24:08 -0400

    c++

--- primes.cpp
+++ primes.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 std::string prime(int x){
+    using namespace std;
     bool f = true;
     for (int i = x / 2; i > 1; i--){
         if(x%i==0){f = false;}
     }
-    return f ? std::to_string(x) + "\n" : "";
+    return f ? to_string(x) + "\n" : "";
 }
 int main(){
     for (int i = 2; i < 100; i++){
