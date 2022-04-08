d788f796e660295899cc8ddd520050c9b648555b
ze-h <chilledlegumes@aol.com>
2022-04-08 10:05:42 -0400

    back at it like a crack addict

--- primes.cs
+++ primes.cs
@@ -1,14 +1,12 @@
 using System;
-namespace primes{
-    bool prime(int x){
-        for (int i = x / 2; i > 1; i--){
-            if(x%i==0){return false;}
-        }
-        return true;
+bool prime(int x){
+    for (int i = x / 2; i > 1; i--){
+        if(x%i==0){return false;}
     }
-    void Main(string[] args){
-        for (int i = 2; i < 100; i++){
-            if(prime(i)){Console.WriteLine(i);}
-        }
+    return true;
+}
+void Main(string[] args){
+    for (int i = 2; i < 100; i++){
+        if(prime(i)){Console.WriteLine(i);}
     }
 }
