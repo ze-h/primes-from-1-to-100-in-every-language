86930494ad1e5c743d22aea55bb227148b8ab757
ze-h <67851652+ze-h@users.noreply.github.com>
2022-04-08 09:35:43 -0400

    ts

---
+++ primes.ts
@@ -0,0 +1,9 @@
+function prime(x){
+    for (let index = 2; index <= x/2; index++) {
+        if (x%index==0) { return false }
+    }
+    return true
+}
+for (let k = 2; k < 100; k++) {
+    if(prime(k)){ console.log(k) }
+}
