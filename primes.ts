146ffd9c7a613810fd9d0baf906cf714611d8c9b
ze-h <chilledlegumes@aol.com>
2022-04-08 11:45:16 -0400

    sync

--- primes.ts
+++ primes.ts
@@ -1,9 +1,9 @@
-function prime(x){
+function p(x){
     for (let index = 2; index <= x/2; index++) {
         if (x%index==0) { return false }
     }
     return true
 }
 for (let k = 2; k < 100; k++) {
-    if(prime(k)){ console.log(k) }
+    if(p(k)){ console.log(k) }
 }
