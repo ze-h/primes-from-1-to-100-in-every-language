c303b6c0ca5fa4af8dfbf18a53bf7f4785f688e0
ze-h <chilledlegumes@aol.com>
2022-04-19 11:52:05 -0400

    asm

---
+++ primes.s
@@ -0,0 +1,102 @@
+	.file	"primes.c"
+	.text
+	.globl	prime
+	.type	prime, @function
+prime:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	-20(%rbp), %eax
+	movl	%eax, %edx
+	shrl	$31, %edx
+	addl	%edx, %eax
+	sarl	%eax
+	movl	%eax, -4(%rbp)
+	jmp	.L2
+.L5:
+	movl	-20(%rbp), %eax
+	cltd
+	idivl	-4(%rbp)
+	movl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L3
+	movl	$0, %eax
+	jmp	.L4
+.L3:
+	subl	$1, -4(%rbp)
+.L2:
+	cmpl	$1, -4(%rbp)
+	jg	.L5
+	movl	$1, %eax
+.L4:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	prime, .-prime
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$2, -4(%rbp)
+	jmp	.L7
+.L9:
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	prime
+	testb	%al, %al
+	je	.L8
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L8:
+	addl	$1, -4(%rbp)
+.L7:
+	cmpl	$99, -4(%rbp)
+	jle	.L9
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
