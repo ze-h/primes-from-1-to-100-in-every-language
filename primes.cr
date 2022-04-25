8c9f08036b5eaa24ec24f810b793bf3326c29a21
ze-h <chilledlegumes@aol.com>
2022-04-24 21:19:06 -0400

    spacing

--- primes.cr
+++ primes.cr
@@ -1,19 +1,19 @@
 def prime(x) : Bool
-  flag = true
-  y = x - 1
-  i = 2
-  until i >= y
-    if x % i == 0
-      flag = false
+    flag = true
+    y = x - 1
+    i = 2
+    until i >= y
+        if x % i == 0
+            flag = false
+        end
+        i += 1
     end
-    i += 1
-  end
-  flag
+    flag
 end
 i = 2
 while i < 100
-  if prime(i) 
-    puts i
-  end
-  i += 1
+    if prime(i) 
+        puts i
+    end
+    i += 1
 end
