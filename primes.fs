31f1054f8f2fb572d521dde609867d13821e02e7
ze-h <chilledlegumes@aol.com>
2022-11-21 10:19:02 -0500

    fantomass

--- primes.fs
+++ primes.fs
@@ -1,7 +1,12 @@
-﻿let prime (x : int) : bool =
+﻿let prime (x: int) : bool =
     let mutable flag = true
-    for i in 2 .. 1 .. (x/2) do
-        if x % i = 0 then flag <- false
+
+    for i in 2..1 .. (x / 2) do
+        if x % i = 0 then
+            flag <- false
+
     flag
-for i in 2 .. 1 .. 100 do
-    if prime(i) then printfn "%i" i
+
+for i in 2..1..100 do
+    if prime (i) then
+        printfn "%i" i
