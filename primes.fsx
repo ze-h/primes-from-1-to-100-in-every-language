f7d4cd39fc1dead486a387370b6b170663ea7125
ze-h <chilledlegumes@aol.com>
2022-11-29 12:59:08 -0500

    better fshart shit

--- primes.fsx
+++ primes.fsx
@@ -1,6 +1,4 @@
-let rec prime (x, y) =
-    y = 1 || (x % y <> 0 && prime (x, y - 1))
+let rec prime x y =
+    y = 1 || (x % y <> 0 && prime x (y - 1))
 
-for i in [ 2..100 ] do
-    if prime (i, i / 2) then
-        printfn "%i" i
+List.filter (fun x -> prime x (x / 2)) [2..100] |> List.iter (printfn "%i")
