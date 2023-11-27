9a32ed1b4d75806dcdc0680cea334c41f191f957
ze-h <ziadhefnawy@gmail.com>
2023-11-27 11:25:35 -0500

    a better racket method

--- primes.rkt
+++ primes.rkt
@@ -1,11 +1,3 @@
 #lang racket
-(define (prm x y)
-  (if (equal? y 1)
-      x
-      (if
-       (equal? (remainder x y) 0)
-       null
-       (prm x (- y 1)))
-      ))
-(for ([i (in-range 100 1 -1)])
-  (if (integer? (prm i (quotient i 2))) (println (prm i (quotient i 2))) null))
+(define prm (lambda (x y) (if (equal? y 1) x (if (equal? (remainder x y) 0) null (prm x (- y 1))))))
+(displayln (filter (lambda (x) (integer? (prm x (quotient x 2)))) (range 2 100)))
